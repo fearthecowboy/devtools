70b720d96389787bb2148c574d7eba899e518d3b
Garrett Serack <gserack@gmail.com>
2012-09-11 13:39:37 -0700

    shuffling and createing skeletons

---
+++ Powershell/Scripting/Commands/StopRestService.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="CoApp Project">
+//     Copyright (c) 2010-2012 Garrett Serack and CoApp Contributors. 
+//     Contributors can be discovered using the 'git log' command.
+//     All rights reserved.
+// </copyright>
+// <license>
+//     The software is licensed under the Apache 2.0 License (the "License")
+//     You may not use the software except in compliance with the License. 
+// </license>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoApp.Scripting.Commands {
+    using System.Management.Automation;
+
+    class StopRestService : Cmdlet {
+    }
+}
